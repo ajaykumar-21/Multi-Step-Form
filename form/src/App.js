8e3572055d0557103b933ad684f0d48053e6ab09
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-08-06 13:36:07 +0530

    create stepper component

---
+++ form/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HorizontalStepper from "./components/Stepper/Stepper";
+
+function App() {
+  return (
+    <div className="App">
+      <HorizontalStepper />
+    </div>
+  );
+}
+
+export default App;
