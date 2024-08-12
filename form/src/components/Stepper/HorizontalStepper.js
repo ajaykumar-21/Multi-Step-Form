e866ba6ee5a512e17c57799fbed8165172bc7263
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-08-12 16:13:30 +0530

    add validation for input field in personalInfo

--- form/src/components/Stepper/HorizontalStepper.js
+++ form/src/components/Stepper/HorizontalStepper.js
@@ -27,16 +27,43 @@ export default function HorizontalStepper() {
 
   // Function to validate form fields based on the current step in the form
   const validateFields = () => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const nameRegex = /^[A-z]*$|^[A-z]+\s[A-z]*$/;
+    const phoneRegex = /^[0-9]{10}$/;
+    const addressRegesx = /^[a-zA-Z0-9._-]/;
     const newErrors = {};
     // Validation for step 0 (Personal Information)
     if (activeStep === 0) {
-      if (!values.name) newErrors.name = "Name is required"; // Check if name is empty
-      if (!values.email) newErrors.email = "Email is required"; // Check if email is empty
-      if (!values.phone) newErrors.phone = "Phone is required"; // Check if phone number is empty
+      if (!values.name) {
+        // Check if name is empty
+        newErrors.name = "Name is required";
+      } else if (!values.name.match(nameRegex)) {
+        // check if name is correct or not
+        newErrors.name = "Enter the correct name";
+      }
+      if (!values.email) {
+        // Check if email is empty
+        newErrors.email = "Email is required";
+      } else if (!values.email.match(emailRegex)) {
+        // check if email is correct or not
+        newErrors.email = "Enter a valid email address";
+      }
+      if (!values.phone) {
+        // Check if phone number is empty
+        newErrors.phone = "Phone is required";
+      } else if (!values.phone.match(phoneRegex)) {
+        // check if phone number is valid or not
+        newErrors.phone = "Enter a valid phone number";
+      }
 
       // Validation for step 1 (Address Information)
     } else if (activeStep === 1) {
-      if (!values.addressLine1) newErrors.addressLine1 = "Address1 is required"; // Check if Address Line 1 is empty
+      if (!values.addressLine1) {
+        newErrors.addressLine1 = "Address1 is required";
+      } else if (!values.addressLine1.match(addressRegesx)) {
+        newErrors.addressLine1 = "Enter correct address";
+      }
+      // Check if Address Line 1 is empty
       if (!values.addressLine2) newErrors.addressLine2 = "Email is required"; // Check if Address Line 2 is empty
       if (!values.city) newErrors.city = "Phone is required"; // Check if city is empty
       if (!values.state) newErrors.state = "Phone is required"; // Check if state is empty
