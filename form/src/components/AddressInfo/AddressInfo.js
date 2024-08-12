f0aab1dbdddfea2732cc9c9f345c146f1ee3c355
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-08-12 18:07:18 +0530

    change the design of input field for addressInfo

--- form/src/components/AddressInfo/AddressInfo.js
+++ form/src/components/AddressInfo/AddressInfo.js
@@ -1,105 +1,100 @@
 import React from "react";
-import {
-  FormControl,
-  InputLabel,
-  Input,
-  Box,
-  FormHelperText,
-} from "@mui/material";
 import "./AddressInfo.css";
 
 function AddressInfo({ values, setValues, errors }) {
   return (
-    <div>
-      <Box component="form">
-        <FormControl variant="standard">
-          <FormControl
-            variant="standard"
-            sx={{ marginBottom: "15px" }}
-            error={!!errors.addressLine1}
+    <div class="form-container">
+      <form>
+        <div class="form-group">
+          <label
+            for="addressLine1"
+            class={errors.addressLine1 ? "label error" : "label"}
           >
-            <InputLabel htmlFor="addressLine1">Address Line 1</InputLabel>
-            <Input
-              id="addressLine1"
-              type="text"
-              value={values.addressLine1}
-              onChange={(e) =>
-                setValues((prev) => ({ ...prev, addressLine1: e.target.value }))
-              }
-              className="inputField"
-            />
-            {errors.addressLine1 && (
-              <FormHelperText>{errors.addressLine1}</FormHelperText>
-            )}
-          </FormControl>
-          <FormControl
-            variant="standard"
-            sx={{ marginBottom: "15px" }}
-            error={!!errors.addressLine2}
+            Address Line 1
+          </label>
+          <input
+            type="text"
+            id="addressLine1"
+            class={errors.addressLine1 ? "inputField error" : "inputField"}
+            value={values.addressLine1}
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, addressLine1: e.target.value }))
+            }
+          />
+          {errors.addressLine1 && (
+            <p class="error-message">{errors.addressLine1}</p>
+          )}
+        </div>
+
+        <div class="form-group">
+          <label
+            for="addressLine2"
+            class={errors.addressLine2 ? "label error" : "label"}
           >
-            <InputLabel htmlFor="addressLine2">Address Line 2</InputLabel>
-            <Input
-              id="addressLine2"
-              type="text"
-              value={values.addressLine2}
-              onChange={(e) =>
-                setValues((prev) => ({ ...prev, addressLine2: e.target.value }))
-              }
-            />
-            {errors.addressLine2 && (
-              <FormHelperText>{errors.addressLine2}</FormHelperText>
-            )}
-          </FormControl>
-          <FormControl
-            variant="standard"
-            sx={{ marginBottom: "15px" }}
-            error={!!errors.city}
-          >
-            <InputLabel htmlFor="city">City</InputLabel>
-            <Input
-              id="city"
-              type="text"
-              value={values.city}
-              onChange={(e) =>
-                setValues((prev) => ({ ...prev, city: e.target.value }))
-              }
-            />
-            {errors.city && <FormHelperText>{errors.city}</FormHelperText>}
-          </FormControl>
-          <FormControl
-            variant="standard"
-            sx={{ marginBottom: "15px" }}
-            error={!!errors.state}
-          >
-            <InputLabel htmlFor="state">State</InputLabel>
-            <Input
-              id="state"
-              type="text"
-              value={values.state}
-              onChange={(e) =>
-                setValues((prev) => ({ ...prev, state: e.target.value }))
-              }
-            />
-            {errors.state && <FormHelperText>{errors.state}</FormHelperText>}
-          </FormControl>
-          <FormControl
-            variant="standard"
-            sx={{ marginBottom: "15px" }}
-            error={!!errors.zip}
-          >
-            <InputLabel htmlFor="zip">Zip</InputLabel>
-            <Input
-              id="zip"
-              type="text"
-              value={values.zip}
-              onChange={(e) =>
-                setValues((prev) => ({ ...prev, zip: e.target.value }))
-              }
-            />
-            {errors.zip && <FormHelperText>{errors.zip}</FormHelperText>}
-          </FormControl>
-        </FormControl>
-      </Box>
+            Address Line 2
+          </label>
+          <input
+            type="text"
+            id="addressLine2"
+            class={errors.addressLine2 ? "inputField error" : "inputField"}
+            value={values.addressLine2}
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, addressLine2: e.target.value }))
+            }
+          />
+          {errors.addressLine2 && (
+            <p class="error-message">{errors.addressLine2}</p>
+          )}
+        </div>
+
+        <div class="form-group">
+          <label for="city" class={errors.city ? "label error" : "label"}>
+            City
+          </label>
+          <input
+            type="text"
+            id="city"
+            class={errors.city ? "inputField error" : "inputField"}
+            value={values.city}
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, city: e.target.value }))
+            }
+          />
+          {errors.city && <p class="error-message">{errors.city}</p>}
+        </div>
+
+        <div class="form-group">
+          <label for="state" class={errors.state ? "label error" : "label"}>
+            State
+          </label>
+          <input
+            type="text"
+            id="state"
+            class={errors.state ? "inputField error" : "inputField"}
+            value={values.state}
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, state: e.target.value }))
+            }
+          />
+          {errors.state && <p class="error-message">{errors.state}</p>}
+        </div>
+
+        <div class="form-group">
+          <label for="zip" class={errors.zip ? "label error" : "label"}>
+            Zip Code
+          </label>
+          <input
+            type="text"
+            id="zip"
+            class={errors.zip ? "inputField error" : "inputField"}
+            value={values.zip}
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, zip: e.target.value }))
+            }
+          />
+          {errors.zip && <p class="error-message">{errors.zip}</p>}
+        </div>
+      </form>
     </div>
   );
 }
--- form/src/components/Confirmation/Confirmation.js
+++ form/src/components/Confirmation/Confirmation.js
@@ -5,14 +5,39 @@ function Confirmation({ values }) {
   return (
     <div className="confirmationPage-container">
       <div className="wrapper">
-        <p>Name: {values.name}</p>
-        <p>Email: {values.email}</p>
-        <p>Phone: {values.phone}</p>
-        <p>Address 1: {values.addressLine1}</p>
-        <p>Address 2: {values.addressLine2}</p>
-        <p>City: {values.city}</p>
-        <p>State: {values.state}</p>
-        <p>Zip: {values.zip}</p>
+        {/* <h1 className="confirmation-title">Confirmation</h1> */}
+        <div className="info-group">
+          <p className="info-label">Name:</p>
+          <p className="info-value">{values.name}</p>
+        </div>
+        <div className="info-group">
+          <p className="info-label">Email:</p>
+          <p className="info-value">{values.email}</p>
+        </div>
+        <div className="info-group">
+          <p className="info-label">Phone:</p>
+          <p className="info-value">{values.phone}</p>
+        </div>
+        <div className="info-group">
+          <p className="info-label">Address 1:</p>
+          <p className="info-value">{values.addressLine1}</p>
+        </div>
+        <div className="info-group">
+          <p className="info-label">Address 2:</p>
+          <p className="info-value">{values.addressLine2}</p>
+        </div>
+        <div className="info-group">
+          <p className="info-label">City:</p>
+          <p className="info-value">{values.city}</p>
+        </div>
+        <div className="info-group">
+          <p className="info-label">State:</p>
+          <p className="info-value">{values.state}</p>
+        </div>
+        <div className="info-group">
+          <p className="info-label">Zip:</p>
+          <p className="info-value">{values.zip}</p>
+        </div>
       </div>
     </div>
   );
